d5397a7f277e913defcf31e9ca67aaac25c0da9c
nessou3060 <benzina.ines98@gmail.com>
2020-11-18 21:01:06 +0100

    « css »

--- src/composant/CheckoutForm.js
+++ src/composant/CheckoutForm.js
@@ -1,86 +1,86 @@
-import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
-import axios from "axios";
-import React, { useState } from "react";
+// import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
+// import axios from "axios";
+// import React, { useState } from "react";
 
-const CheckoutForm = ({ name, price }) => {
-  const elements = useElements();
-  const stripe = useStripe();
+// const CheckoutForm = ({ name, price }) => {
+//   const elements = useElements();
+//   const stripe = useStripe();
 
-  const [completed, setCompleted] = useState(false);
+//   const [completed, setCompleted] = useState(false);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
 
-    const cardElement = elements.getElement(CardElement);
+//     const cardElement = elements.getElement(CardElement);
 
-    const stripeResponse = await stripe.createToken(cardElement);
-    console.log(stripeResponse);
+//     const stripeResponse = await stripe.createToken(cardElement);
+//     console.log(stripeResponse);
 
-    const stripeToken = stripeResponse.token.id;
+//     const stripeToken = stripeResponse.token.id;
 
-    const response = await axios.post(
-      "https://lereacteur-vinted-api.herokuapp.com/payment",
-      {
-        token: stripeToken,
-        title: name,
-        amount: price,
-      }
-    );
-    console.log(response.data);
+//     const response = await axios.post(
+//       "https://lereacteur-vinted-api.herokuapp.com/payment",
+//       {
+//         token: stripeToken,
+//         title: name,
+//         amount: price,
+//       }
+//     );
+//     console.log(response.data);
 
-    if (response.data.status === "c'est ok") {
-      setCompleted(true);
-    }
-  };
-  const fraisProtection = 0.4;
-  const fraisPort = 0.8;
-  const total = (fraisPort + fraisProtection + price).toFixed(2);
-  return (
-    <>
-      {!completed ? (
-        <div className="container-form">
-          <form onSubmit={handleSubmit}>
-            <div className="">
-              <div className="">
-                <h2>Rèsumè de la commande</h2>
-                <p>{name}</p>
-              </div>
-              <div className="">
-                <p>Commande</p>
-                <p>{price} €</p>
-              </div>
-              <div className="">
-                <p>Frais de protection Acheteurs</p>
-                <p>{fraisProtection.toFixed(2)} £</p>
-              </div>
-              <div className="">
-                <p>Frais de Port</p>
-                <p>{fraisPort.toFixed(2)} £</p>
-              </div>
-              <hr />
-            </div>
-            <div className="">
-              <div className="">
-                <h2>Total</h2>
-                <div>{total} £</div>
-              </div>
-              <p className="">
-                Il ne nous reste qu'une seule ètape pour vous offrir {name}.
-                vous allez payer <strong>{total} €</strong> (frais deprotection
-                et frais de port inclus)
-              </p>
-            </div>
-            <hr />
-            <CardElement className="payement" />
-            <button className="pay" type="submit">
-              Payer
-            </button>
-          </form>
-        </div>
-      ) : (
-        <Result name={name} total={total} />
-      )}
-    </>
-  );
-};
-export default CheckoutForm;
+//     if (response.data.status === "c'est ok") {
+//       setCompleted(true);
+//     }
+//   };
+//   const fraisProtection = 0.4;
+//   const fraisPort = 0.8;
+//   const total = (fraisPort + fraisProtection + price).toFixed(2);
+//   return (
+//     <>
+//       {!completed ? (
+//         <div className="container-form">
+//           <form onSubmit={handleSubmit}>
+//             <div className="">
+//               <div className="">
+//                 <h2>Rèsumè de la commande</h2>
+//                 <p>{name}</p>
+//               </div>
+//               <div className="">
+//                 <p>Commande</p>
+//                 <p>{price} €</p>
+//               </div>
+//               <div className="">
+//                 <p>Frais de protection Acheteurs</p>
+//                 <p>{fraisProtection.toFixed(2)} £</p>
+//               </div>
+//               <div className="">
+//                 <p>Frais de Port</p>
+//                 <p>{fraisPort.toFixed(2)} £</p>
+//               </div>
+//               <hr />
+//             </div>
+//             <div className="">
+//               <div className="">
+//                 <h2>Total</h2>
+//                 <div>{total} £</div>
+//               </div>
+//               <p className="">
+//                 Il ne nous reste qu'une seule ètape pour vous offrir {name}.
+//                 vous allez payer <strong>{total} €</strong> (frais deprotection
+//                 et frais de port inclus)
+//               </p>
+//             </div>
+//             <hr />
+//             <CardElement className="payement" />
+//             <button className="pay" type="submit">
+//               Payer
+//             </button>
+//           </form>
+//         </div>
+//       ) : (
+//         <Result name={name} total={total} />
+//       )}
+//     </>
+//   );
+// };
+// export default CheckoutForm;
